641da2a4488de2f2b836843f3c4fac439fde85af
athanrbl <148010540+athanrbl@users.noreply.github.com>
2024-03-11 16:48:48 +0200

    Polybius Prototype logic
    Experimenting with the polybius cipher using lists and indexes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send("Home")
 })
 
+app.get('/login', (req, res) => {
+  res.send("Login") //send data to dynamically load account
+})
+
 app.listen(port, () => {
   console.log(`Running on localhost with port ${port}`)
 })
